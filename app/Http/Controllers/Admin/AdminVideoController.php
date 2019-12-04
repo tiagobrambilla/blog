7633d938deb8919608a3b609c975c7c3876c490e
Tiago Brambilla <30738700+tiagobrambilla@users.noreply.github.com>
2019-12-04 13:26:39 -0300

    Alterações Última Aula

--- app/Http/Controllers/Admin/AdminVideoController.php
+++ app/Http/Controllers/Admin/AdminVideoController.php
@@ -1,4 +1,5 @@
 <?php
+
 namespace App\Http\Controllers\Admin;
 
 use App\Http\Controllers\Controller;
@@ -7,66 +8,104 @@ use App\Video;
 
 class AdminVideoController extends Controller
 {
-    
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
     public function index()
     {
         $videos = Video::all();
-        
-        return view('admin.Videos.index',compact('videos'));
+
+        return view('admin.videos.index',compact('videos'));
     }
-    
-    
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
     public function create()
     {
-        return view('admin.Videos.create');
+        return view('admin.videos.create');
     }
-    
-    
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
     public function store(Request $request)
     {
         $video = new Video;
+
         $video->titulo = $request->titulo;
-        $video->descricao =$request->descricao;
+        $video->descricao = $request->descricao;
         $video->link = $request->link;
+
         $video->save();
-        
+
         return redirect('admin/videos');
-        
+
     }
-    
-    
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
     public function show($id)
     {
         //
     }
-    
-    
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
     public function edit($id)
     {
         $video = Video::find($id);
-        return view('admin.Videos.edit', compact('video'));
+
+        return view('admin.videos.edit', compact('video'));
     }
-    
-    
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
     public function update(Request $request, $id)
     {
         $video = Video::find($id);
+
         $video->titulo = $request->titulo;
-        $video->descricao =$request->descricao;
+        $video->descricao = $request->descricao;
         $video->link = $request->link;
+
         $video->save();
-        
+
         return redirect('admin/videos');
-        
     }
-    
-    
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
     public function destroy($id)
     {
         $video = Video::find($id);
-        
+
         $video->delete();
-        
+
         return redirect('admin/videos');
     }
 }
