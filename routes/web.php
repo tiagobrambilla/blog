aa7d0ff6ec2e436bd71eb4e32c69ff923cf49708
Tiago Brambilla <30738700+tiagobrambilla@users.noreply.github.com>
2019-12-04 13:33:09 -0300

    Alterações Última Aula

--- routes/web.php
+++ routes/web.php
@@ -1,10 +1,42 @@
 <?php
 
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
 Route::get('/', function () {
-    return view('site/welcome');
+	$nome = 'kamile';
+    return view('site/welcome', compact('nome'));
 });
-Route::namespace('Admin')->prefix('admin')->group(function(){
+
+Auth::routes();
+
+
+Route::middleware('auth')->namespace('Admin')->prefix('admin')->group(function () {
+
 	Route::resource('posts','AdminPostController');
+
 	Route::resource('videos','AdminVideoController');
 });
+
+Route::get('inicio', 'HomeController@index');
+
+
+
+
+
+
+
+
+
+
+
+
 
